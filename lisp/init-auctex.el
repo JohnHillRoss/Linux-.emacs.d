d224f78d366251781a2f8c70c5e4e0b70bd0f852
JohnHihllRoss <JohnHillRoss@163.com>
2019-01-18 13:39:07 +0800

    Add forward and inverse search

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -7,52 +7,51 @@
 ;;默认关闭tex
 (setq-default TeX-master nil)
 
-;;自定义编辑LaTeX模式
-(mapc (lambda (mode)
-	(add-hook 'LaTeX-mode-hook mode)
-	)
-      (list
-       'auto-complete-mode
-       'auto-fill-mode
-       'LaTeX-math-mode
-       'turn-on-reftex
-       'linum-mode
-       ))
+;;开启pdf阅读模式
+(setq TeX-PDF-mode t)
 
-;;自定义编辑LaTeX方法
-(add-hook 'LaTeX-mode-hook(lambda ()
-			    (setq TeX-auto-untabify t     ; remove all tabs before saving
-				  TeX-engine 'xetex       ; use xelatex default
-				  TeX-show-compilation t  ; display compilation windows
-				  )
-			    (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
-			    (setq TeX-save-query nil)
-			    (imenu-add-menubar-index)
-			    (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)
-			    ))
-;;修改pdf阅读器
-(setq TeX-view-program-list '(("Evince" "evince %o")))
-(cond
- ;;windows平台使用SumatraPDF
- ((eq system-type 'windows-nt)
-  (add-hook 'LaTeX-mode-hook
-	    (lambda ()
-	      (setq TeX-view-program-selection '((output-pdf "SumatraPDF")
-                                                 (output-dvi "Yap")
-						 )))))
- ;;linux平台使用Evince
- ((eq system-type 'gnu/linux)
-  (add-hook 'LaTeX-mode-hook
-            (lambda ()
-              (setq TeX-view-program-selection '((output-pdf "Evince")
-                                                 (output-dvi "Evince")
-						 ))))))
-;;编辑引擎改为XeLaTeX
-(add-hook 'LaTeX-mode-hook (lambda()
-			     (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-			     (setq TeX-command-default "XeLaTeX")
-			     (setq TeX-save-query  nil )
-			     (setq TeX-show-compilation t)
-			     ))
+;;开启反向搜索
+(setq TeX-source-correlate-mode t)
+
+;;设置反向搜索方式为synctex
+(setq TeX-source-correlate-method 'synctex)
+
+;;设置pdf阅读器
+(setq TeX-view-program-list
+      '(("Sumatra PDF" ("\"d:/Software/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+                        (mode-io-correlate " -forward-search %b %n -inverse-search \"d:/Software/Emacs/emacs-26.1-x86_64/bin/emacsclientw.exe -n +%%l %%f\"") " %o"))))
+(eval-after-load 'tex
+  '(progn
+     (assq-delete-all 'output-pdf TeX-view-program-selection)
+     (add-to-list 'TeX-view-program-selection '(output-pdf "Sumatra PDF"))))
+
+;;设置编译引擎为XeLaTeX
+(add-hook 'LaTeX-mode-hook
+          #'(lambda ()
+              (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+	      (setq TeX-command-extra-options "-file-line-error -shell-escape")
+	      (setq TeX-command-default "XeLaTeX")
+	      (setq TeX-save-query  nil)
+	      ))
+
+(setq TeX-view-program-selection '(((output-dvi style-pstricks) "dvips and start") (output-dvi "Yap") (output-pdf "Sumatra PDF") (output-html "start")))
+
+(setq TeX-output-view-style '(("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "dvips %d -o && start \"\" %f") ("^dvi$" "." "yap -1 %dS %d") ("^pdf$" "." "d:/Software/SumatraPDF/SumatraPDF.exe -reuse-instance \"\" %o") ("^html?$" "." "start \"\" %o")))
+
+;;开启正向搜索
+(setq TeX-source-correlate-start-server t)
+
+;;开启正向搜索,shift+鼠标右键
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+	    (local-set-key (kbd "<S-mouse-3>") #'TeX-view)
+	    ))
+
+;;在菜单栏添加index,math
+(add-hook 'LaTeX-mode-hook
+	  (lambda ()
+	    (imenu-add-menubar-index)
+	    (LaTeX-math-mode)
+	    ))
 
 (provide 'init-auctex)
--- lisp/init-reftex.el
+++ lisp/init-reftex.el
@@ -1,19 +1,16 @@
-;;自定义reftex
+;;开启reftex
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
+;;自定义配置reftex
 (setq reftex-plug-into-AUCTeX t)
-
 (setq reftex-enable-partial-scans t)
-
 (setq reftex-save-parse-info t)
-
 (setq reftex-use-multiple-selection-buffers t)
 
+;;自定义自动加载reftex模式
 (autoload 'reftex-mode "reftex" "RefTex Minor Mode" t)
-
 (autoload 'turn-on-reftex "reftex" "RefTex Minor Mode" nil)
-
 (autoload 'reftex-citation "reftex-cite" "Make citation" nil)
-
 (autoload 'reftex-index-phrases-mode "reftex-index" "Phrase mode" t)
 
 ;;*toc*buffer在左侧。 
