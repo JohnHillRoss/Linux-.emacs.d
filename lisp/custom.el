d5611345dc7a3fca02a6ffbf327aa104719231da
JohnHihllRoss <JohnHillRoss@163.com>
2019-01-19 02:00:11 +0800

    Debug

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 1))
+ '(company-minimum-prefix-length 1)
+ '(package-selected-packages
+   (quote
+    (auctex company hungry-delete swiper counsel smartparens popwin darkokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
